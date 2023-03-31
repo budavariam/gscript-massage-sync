8044dede3cec77901b682310a588d95266f1dd90
Mátyás Budavári <budavariam@gmail.com>
2023-03-31 07:26:49 +0200

    Encapsulate only the secret valjues from the config

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Google Docs Calendar Sync
 
+Google Apps Script to create up to date calendar events based on a Google Drive document for the current week.
 
+## Getting started
+
+- see [clasp docs](https://github.com/google/clasp)
+- see the [DEV](./DEV.md) Docs
---
+++ src/modules/config.ts
@@ -0,0 +1,10 @@
+namespace Config {
+    export const calendarId = Secret.calendarId;
+    export const sheetId = Secret.sheetId;
+    export const companyEmailDomain = Secret.companyEmailDomain;
+    export const nicknameMapping = Secret.nicknameMapping;
+    export const tempMasseurData = Secret.tempMasseurData
+    export const flags = {
+        ignorePastDays: false
+    }
+}
